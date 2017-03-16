38e352bef23da0462cd67f80979b699137ce5757
adam123 <wjw3323@live.com>
2017-03-16 11:15:36 +0800

    add redis code

--- redis/client.php
+++ redis/client.php
@@ -7,16 +7,10 @@
  */
 
 $data = '{"code":0,"msg":"ok","info":["hello","world"]}';
-$key = ftok(__DIR__.DIRECTORY_SEPARATOR.'server.php', 'a');
-$message_queue = msg_get_queue($key, 0666);
-//向消息队列中写
-$i = 1000;
-while ($i > 0){
-    $i --;
-    msg_send($message_queue, 1, $data);
-
+$client = new Redis();
+$client->connect('127.0.0.1');
+$queue_name = 'test';
+for ($i = 0; $i < 1000; $i ++){
+    $client->lPush('test', $data);
 }
-
-if (isset($_GET['end'])){
-    msg_remove_queue($message_queue);
-}
+$client->close();
--- redis/server.php
+++ redis/server.php
@@ -6,11 +6,12 @@
  * Time: 上午 9:57
  */
 
-$data = '{"code":0,"msg":"ok","info":["hello","world"]}';
-$key = ftok(__FILE__, 'a');
-$message_queue = msg_get_queue($key, 0666);
-//从消息队列中读
+$client = new Redis();
+$client->connect('127.0.0.1');
+$queue_name = 'test';
 while (1){
-    msg_receive($message_queue, 0, $message_type, 1024, $message, true, MSG_IPC_NOWAIT);
+    if($client->lLen('test') > 0){
+        $client->lPop('test');
+    }
 }
-msg_remove_queue($message_queue);
+$client->close();
